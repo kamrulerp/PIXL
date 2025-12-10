2f250baff087421cc7802ed865b65834e715c1ad
kamrulerp <kamrulerp@gmail.com>
2025-12-10 07:24:49 +0600

    Extracted - post/reply form

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -293,64 +293,14 @@
                     class="size-10 object-cover"
                   />
                 </a>
-
-                <form class="grow" action="">
-                  <label class="sr-only" for="reply">Reply body</label>
-                  <textarea
-                    class="w-full resize-none text-sm"
-                    name="reply"
-                    id="reply"
-                    rows="5"
-                    placeholder="Reply to Michael's post"
-                  ></textarea>
-
-                  <div class="flex items-center justify-between gap-4">
-                    <div class="flex gap-4">
-                      <button type="button">
-                        <svg
-                          width="20"
-                          viewBox="0 0 20 20"
-                          class="text-icon-btn fill-current"
-                        >
-                          <path d="M15 1H5V3H15V1Z"></path>
-                          <path d="M17 3H15V17H17V3Z"></path>
-                          <path d="M5 3H3V17H5V3Z"></path>
-                          <path d="M9 15H7V17H9V15Z"></path>
-                          <path d="M7 17H5V19H7V17Z"></path>
-                          <path d="M11 12H9V15H11V12Z"></path>
-                          <path d="M13 15H11V17H13V15Z"></path>
-                          <path d="M15 17H13V19H15V17Z"></path>
-                        </svg>
-                      </button>
-                      <button type="button">
-                        <svg
-                          width="20"
-                          viewBox="0 0 20 20"
-                          class="text-icon-btn fill-current"
-                          shape-rendering="crispEdges"
-                          style="image-rendering: pixelated"
-                        >
-                          <path d="M16 9V11L18 11V9L16 9Z"></path>
-                          <path d="M13 7V9L16 9V7L13 7Z"></path>
-                          <path d="M13 13V11L16 11V13L13 13Z"></path>
-                          <path d="M10 5V7L13 7V5L10 5Z"></path>
-                          <path d="M10 15V13L13 13V15L10 15Z"></path>
-                          <path d="M7 3V5L10 5V3L7 3Z"></path>
-                          <path d="M7 17V15L10 15V17L7 17Z"></path>
-                          <path d="M4 1V3L7 3V1L4 1Z"></path>
-                          <path d="M4 19V17L7 17V19L4 19Z"></path>
-                          <path d="M2 3L2 17L4 17L4 3L2 3Z"></path>
-                        </svg>
-                      </button>
-                    </div>
-                    <button
-                      class="bg-pixl/10 hover:bg-pixl/15 active:bg-pixl/20 text-pixl border border-transparent px-2 py-1"
-                      type="submit"
-                    >
-                      Post
-                    </button>
-                  </div>
-                </form>
+              <!-- reply form -->
+                @include('partials.post-form', [
+                    'fieldName' => 'reply',
+                    'labelText' => 'Reply body',
+                    'placeholder' => "Reply to Michael's post",
+                    'rows' => 5,
+                    'buttonClasses' =>'bg-pixl/10 hover:bg-pixl/15 active:bg-pixl/20 text-pixl border border-transparent px-2 py-1',
+                ])
               </div>
             </div>
             <!--Threaded replies-->
